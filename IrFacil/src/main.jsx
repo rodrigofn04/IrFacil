aa3971ee5b626d1410a781c1b684851bbcbd2941
Rodrigo <rodrigo_ferreira20.04@hotmail.com>
2025-09-03 22:15:03 -0300

    hosting stuff

--- IrFacil/src/main.jsx
+++ IrFacil/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import SaibaMais from "./pages/SaibaMais";
 import Contato from "./pages/Contato";
 import "./index.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
